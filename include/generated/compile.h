756da7610a9e76c889e32dc5fe3a52f92f4adcf8
Adam Honse <calcprogrammer1@gmail.com>
2012-01-22 23:28:59 -0600

    Added initial Archos support files, a few changes, does not compile

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#0 PREEMPT Sun Jan 22 23:18:21 CST 2012"
+#define LINUX_COMPILE_BY "adam"
+#define LINUX_COMPILE_HOST "Adam-Ubuntu1110-VB"
+#define LINUX_COMPILER "gcc version 4.6.1 (Ubuntu/Linaro 4.6.1-9ubuntu3) "
